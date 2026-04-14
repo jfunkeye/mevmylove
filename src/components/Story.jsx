4c11af9382bd19aabb6a2b143fb40fe62bbcdfe4
JOSEPH ANDREW FUNKEYE <jfunkeye@gmail.com>
2026-04-14 23:31:07 +0100

    Update portrait image path in Story component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import portraitImage from '../assets/image1.jpg'
+import portraitImage from '../assets/1.jpg'
 
 const Story = () => {
   const sectionRef = useRef(null)
