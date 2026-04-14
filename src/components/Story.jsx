be24e0f887614d2791819afbacc384a301b55d48
JOSEPH ANDREW FUNKEYE <jfunkeye@gmail.com>
2026-04-14 23:36:08 +0100

    Update image import path in Story component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import Image from '../assets/2.jpg'
+import Image from '../assets/images2.jpg'
 
 const Story = () => {
   const sectionRef = useRef(null)
